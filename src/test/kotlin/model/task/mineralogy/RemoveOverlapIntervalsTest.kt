15b797fc1e5b6f89190e823f05622a6e0e69de6c
alexrnov <a.novopashin5@gmail.com>
2019-10-21 12:08:27 +0900

    create algorithm for remove overlap probes

--- src/test/kotlin/model/task/mineralogy/RemoveOverlapIntervalsTest.kt
+++ src/test/kotlin/model/task/mineralogy/RemoveOverlapIntervalsTest.kt
@@ -35,4 +35,44 @@ internal class RemoveOverlapIntervalsTest {
     table.forEach { task.perform(it) }
     //task.writeData()
   }
+
+  @Test
+  fun algorithm() {
+    val probesWithMSD = listOf(
+            mapOf("ID" to "177659", "От" to "71.6", "До" to "72.6"),
+            mapOf("ID" to "177659", "От" to "72.6", "До" to "73.4"),
+            mapOf("ID" to "177659", "От" to "73.4", "До" to "74.3"),
+            mapOf("ID" to "177659", "От" to "74.3", "До" to "75.1"),
+            mapOf("ID" to "177659", "От" to "75.1", "До" to "76"),
+            mapOf("ID" to "177659", "От" to "76", "До" to "83"))
+    val emptyProbes = listOf(
+            mapOf("ID" to "177659", "От" to "54.2", "До" to "54.8"),
+            mapOf("ID" to "177659", "От" to "67.7", "До" to "68.2"),
+            mapOf("ID" to "177659", "От" to "71", "До" to "71.6"),
+            mapOf("ID" to "177659", "От" to "71", "До" to "76"),
+            mapOf("ID" to "177659", "От" to "76", "До" to "77"),
+            mapOf("ID" to "177659", "От" to "77", "До" to "78.2"),
+            mapOf("ID" to "177659", "От" to "78.2", "До" to "79.1"),
+            mapOf("ID" to "177659", "От" to "81.1", "До" to "82.2"))
+    fun f(fromEmpty: Double, toEmpty: Double) {
+      for (probeWithMSD in probesWithMSD) {
+        val fromMSD = probeWithMSD["От"]?.toDouble() ?: 0.0
+        val toMSD = probeWithMSD["До"]?.toDouble() ?: 0.0
+        when {
+          (toMSD <= fromEmpty || fromMSD >= toEmpty) -> println("Интервал с МСА за пределами пустой пробы")
+          (fromMSD <= fromEmpty && toMSD >= toEmpty) -> println("Интервал с МСА полностью перекрывает пустой интервал")
+          (fromMSD > fromEmpty && toMSD < toEmpty) -> println("Интервал с МСА лежит внутри пустого интревала")
+          (toMSD > fromEmpty && fromMSD < fromEmpty) -> println("Интервал с МСА перекрывает пустой интервал сверху")
+          (fromMSD < toEmpty && toMSD >= toEmpty) -> println("Интервал с МСА перекрывает пустую пробу снизу")
+        }
+      }
+    }
+
+    for (emptyProbe in emptyProbes) {
+      val fromEmpty = emptyProbe["От"]?.toDouble() ?: 0.0
+      val toEmpty = emptyProbe["До"]?.toDouble() ?: 0.0
+      f(fromEmpty, toEmpty)
+    }
+
+  }
 }
